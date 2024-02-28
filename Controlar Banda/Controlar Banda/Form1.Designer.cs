415b1dcd5e2b296b40898151e430b5aab626c89a
Ramón9988 <124377639+Ender9988@users.noreply.github.com>
2024-02-27 22:56:20 -0600

    Add files via upload
    Aplicacion en donde se encuentra la interfaz de la banda móvil

---
+++ Controlar Banda/Controlar Banda/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace Controlar_Banda
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1Led = new System.Windows.Forms.Label();
+            this.botonEncender = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Contador = new System.Windows.Forms.Label();
+            this.botonApagar = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1Led
+            // 
+            this.label1Led.BackColor = System.Drawing.Color.Red;
+            this.label1Led.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1Led.Font = new System.Drawing.Font("8-bit Arcade In", 44.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1Led.Location = new System.Drawing.Point(279, 75);
+            this.label1Led.Name = "label1Led";
+            this.label1Led.Size = new System.Drawing.Size(118, 50);
+            this.label1Led.TabIndex = 0;
+            this.label1Led.Text = "OFF";
+            this.label1Led.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // botonEncender
+            // 
+            this.botonEncender.BackColor = System.Drawing.Color.Gold;
+            this.botonEncender.Font = new System.Drawing.Font("8-bit Arcade In", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonEncender.Location = new System.Drawing.Point(31, 159);
+            this.botonEncender.Name = "botonEncender";
+            this.botonEncender.Size = new System.Drawing.Size(234, 93);
+            this.botonEncender.TabIndex = 1;
+            this.botonEncender.Text = "Encender";
+            this.botonEncender.UseVisualStyleBackColor = false;
+            this.botonEncender.Click += new System.EventHandler(this.botonEncender_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.Desktop;
+            this.groupBox1.Controls.Add(this.label1Led);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.botonApagar);
+            this.groupBox1.Controls.Add(this.botonEncender);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Font = new System.Drawing.Font("8-bit Arcade In", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 23);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(794, 380);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Enabled = false;
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(279, 147);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(509, 222);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Contador
+            // 
+            this.Contador.AutoSize = true;
+            this.Contador.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.Contador.Font = new System.Drawing.Font("8-bit Arcade In", 80.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Contador.Location = new System.Drawing.Point(229, 36);
+            this.Contador.Name = "Contador";
+            this.Contador.Size = new System.Drawing.Size(85, 72);
+            this.Contador.TabIndex = 3;
+            this.Contador.Text = "0";
+            // 
+            // botonApagar
+            // 
+            this.botonApagar.BackColor = System.Drawing.Color.Gold;
+            this.botonApagar.Font = new System.Drawing.Font("8-bit Arcade In", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonApagar.Location = new System.Drawing.Point(30, 258);
+            this.botonApagar.Name = "botonApagar";
+            this.botonApagar.Size = new System.Drawing.Size(235, 93);
+            this.botonApagar.TabIndex = 2;
+            this.botonApagar.Text = "Apagar";
+            this.botonApagar.UseVisualStyleBackColor = false;
+            this.botonApagar.Click += new System.EventHandler(this.botonApagar_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(41, 34);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(135, 91);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.groupBox2.Controls.Add(this.pictureBox2);
+            this.groupBox2.Controls.Add(this.Contador);
+            this.groupBox2.Font = new System.Drawing.Font("8-bit Arcade In", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(403, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(378, 131);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Conteo de Cajas";
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.label1.Font = new System.Drawing.Font("8-bit Arcade In", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(794, 139);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Estado De \r\nLa Banda:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.LightBlue;
+            this.label2.Font = new System.Drawing.Font("8-bit Arcade In", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(232, 147);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 43);
+            this.label2.TabIndex = 7;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.label3.Location = new System.Drawing.Point(14, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(259, 222);
+            this.label3.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(818, 415);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Control banda";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1Led;
+        private System.Windows.Forms.Button botonEncender;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button botonApagar;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label Contador;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Controlar Banda/Controlar Banda/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace Controlar_Banda
+{
+    public partial class Form1 : Form
+    {
+
+        //Variable que llevara el conteo de las cajas
+        int contador = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+        //Metodo que recibi el caracter por medio del puerto USB
+        private void SerialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            // Lee el carácter recibido del puerto COM
+            char CaracterRecibido = (char)serialPort1.ReadByte();
+
+            // Si el carácter recibido es '1', incrementa el contador y actualiza el numero
+            if (CaracterRecibido == '1')
+            {
+                //Le aumenta 1 a la variable contador
+                contador++;
+                //Metodo que actualiza el valor del contador
+                Invoke(new Action(() => { Contador.Text = contador.ToString(); }));
+            }
+        }
+
+
+        //Metodo que se usa cuando se presiona el boton de encender
+        private void botonEncender_Click(object sender, EventArgs e)
+        {
+            try//Si todo se ejecuta correctamente
+            {
+
+                // Suscribe al evento DataReceived al manejador SerialPort1_DataReceived
+                serialPort1.DataReceived += SerialPort1_DataReceived;
+
+                //Enviar el caracter al micro
+                serialPort1.Write("E");
+                label1Led.BackColor = Color.Lime; //Cambia el fondo a verde
+                label1Led.Text = "ON"; //Cambia el texto a ON
+                botonApagar.Enabled = true; //Habilita el boton de apagar
+                botonEncender.Enabled = false; //Deshabilita el boton de encender
+
+                pictureBox1.Enabled = true; //Hace que se mueva la imagen GIF
+
+            }
+            catch (Exception error)//En caso de algun error muestra el mensaje
+            {
+                MessageBox.Show(error.Message);
+            }
+
+
+
+        }
+
+        //Se ejecuta cuando se inicia la aplicacion
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try //En caso que todo funcione bien
+            {
+
+                botonApagar.Enabled = false; //Deshabilita el boton de apagar
+                serialPort1.PortName = "COM7"; //Abre el puero USB
+                serialPort1.Open(); //Abre el puerto para la comunicacion serial
+
+            }
+            catch (Exception error)//En caso de algun error muestre el mensaje
+            {
+                MessageBox.Show(error.Message);
+
+            }
+
+
+
+
+        }
+
+        //Cuando se cierra la aplicacion 
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (serialPort1.IsOpen) //En el caso de estar abierto el puerto 
+            {
+                try//Si todo se ejecuta bien
+                {
+                    serialPort1.Close();//Se ciera el puero
+                }
+                catch (Exception error) //En caso de un error se muestra el mensaje
+                {
+                    MessageBox.Show(error.Message);
+                }
+            }
+        }
+
+        //Metodo que se aplica cuando se presiona el boton de apagar
+        private void botonApagar_Click(object sender, EventArgs e)
+        {
+            try //Si todo sale bien
+            {
+                //Enviar codigo al micro el caracter A
+                serialPort1.Write("A");
+                label1Led.BackColor = Color.Red; //Cambia el color del fondo del texto a rojo
+                label1Led.Text = "OFF"; //Cambia el texto a OFF
+                botonApagar.Enabled = false; //Deshabilita el boton de Apagar
+                botonEncender.Enabled = true;//Habilita el boton de apagar
+
+
+                contador = 0; // Reinicia el contador al presionar "Apagar"
+                Contador.Text = "0"; // Actualiza el Label del contador
+
+                pictureBox1.Enabled = false; //Pausa la imagen GIF
+            }
+            catch (Exception error) //Muestra el error en el caso de haberlo
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
